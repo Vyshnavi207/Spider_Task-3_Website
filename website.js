3e43ad17b594b9c59ea0e33485476b0457df548e
vyshnavi <vyshnavi.madikonda@gmail.com>
2021-08-19 16:39:05 +0530

    Added User and Product details

--- website.js
+++ website.js
@@ -13,3 +13,39 @@ function myaccount(para){
         console.log(para);
     }  
 }
+const select = document.getElementById("select");
+const closed = document.getElementById("closed");
+const another =document.getElementById("another");
+const selec = document.getElementById("selec");
+const anothe =document.getElementById("anothe");
+function apply(para){
+    if(para=='open'){
+        select.style.display = "block";
+        select.style.display = "flex";
+        another.style.display ="block";
+        another.style.display = "flex";
+        
+    }
+    else if(para=='move'){
+        window.location.href='#auctioneer';
+        select.style.display = "none";
+        another.style.display ="none";
+
+    }
+}
+function appl(para){
+    if(para=='ope'){
+        selec.style.display = "block";
+        selec.style.display = "flex";
+        anothe.style.display ="block";
+        anothe.style.display = "flex";
+        
+    }
+    else if(para=='mov'){
+        window.location.href='#auctioneer';
+        selec.style.display = "none";
+        anothe.style.display ="none";
+
+    }
+}
+const make= document.getElementById("make");
